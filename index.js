7bc883ec5f3f741665911e73d8b19cb40bbd514b
Sandun Tharuka <sandun.suntws@gmail.com>
2023-02-21 00:36:11 +0530

    create module

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-require('./modules/add.js')
+
+
+const superhero =require('./modules/super-hero.js')
+
+
+console.log(superhero.getName());
+superhero.setName('tharuka')
+console.log(superhero.getName());
+
